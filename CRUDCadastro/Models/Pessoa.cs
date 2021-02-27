a399927b0d965e4c851953dc3ed38132e8e4b876
leonardogomescbp <leonardogomescbp@gmail.com>
2021-02-27 04:36:07 -0300

    Primeiro Commit

---
+++ CRUDCadastro/Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CRUDCadastro.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string CPF { get; set; }
+  
+        public int Idade { get; set; }
+        public string Telefone { get; set; }
+
+
+    }
+}
